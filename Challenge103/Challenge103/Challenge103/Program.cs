e94ceb7ef9fd60940489318aa82931b6b2d308c4
Daniel <danlypemol@gmail.com>
2021-01-23 17:53:36 +0100

    Made solutions for challenges 103 & 105

--- Challenge103/Challenge103/Challenge103/Program.cs
+++ Challenge103/Challenge103/Challenge103/Program.cs
@@ -10,6 +10,33 @@ namespace Challenge103
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Write a line that is either all lowercase or all uppercase.");
+            Console.WriteLine("We'll know if it isn't.");
+            CheckString(Console.ReadLine());
+        }
+
+        static void CheckString(string input)
+        {
+            int upperCaseCount = 0;
+            char[] characters = input.ToCharArray();
+            foreach (char item in characters)
+            {
+                if (Char.IsUpper(item))
+                {
+                    upperCaseCount++;
+                }
+            }
+
+            if (upperCaseCount == characters.Length || upperCaseCount == 0)
+            {
+                Console.WriteLine("Yup. " + input + " is indeed what I asked for.");
+            }
+            else
+            {
+                Console.WriteLine("You just had to try, didn't you? " + input + " is a sorry mix of upper AND lower case.");
+            }
+
+            CheckString(Console.ReadLine());
         }
     }
 }
--- Challenge105/Challenge105/Challenge105/Program.cs
+++ Challenge105/Challenge105/Challenge105/Program.cs
@@ -10,6 +10,76 @@ namespace Challenge105
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the numerical value of a month:");
+            CheckMonth(Console.ReadLine());
+        }
+
+        static void CheckMonth (string input)
+        {
+            //Making sure a format exception won't happen.
+            try
+            {
+                int parsedInput = int.Parse(input);
+                string month;
+                if (parsedInput <= 12 && parsedInput > 0)
+                {
+                    //This is probably a mess.
+                    switch (parsedInput)
+                    {
+                        case 1:
+                            month = "January";
+                            break;
+                        case 2:
+                            month = "February";
+                            break;
+                        case 3:
+                            month = "March";
+                            break;
+                        case 4:
+                            month = "April";
+                            break;
+                        case 5:
+                            month = "May";
+                            break;
+                        case 6:
+                            month = "June";
+                            break;
+                        case 7:
+                            month = "July";
+                            break;
+                        case 8:
+                            month = "August";
+                            break;
+                        case 9:
+                            month = "September";
+                            break;
+                        case 10:
+                            month = "October";
+                            break;
+                        case 11:
+                            month = "November";
+                            break;
+                        case 12:
+                            month = "December";
+                            break;
+                        default:
+                            month = "Not a month";
+                            break;
+                    }
+
+                    Console.WriteLine("The number input corresponds to the month of: " + month);
+                }
+                else
+                {
+                    Console.WriteLine(input + " isn't a month.");
+                }
+                CheckMonth(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Your input is not a number.");
+                CheckMonth(Console.ReadLine());
+            }
         }
     }
 }
